042a4f4383d2b1bf11494792dfdb817008c94cec
Ziad Siddique <zssiddique@gmail.com>
2018-11-29 00:06:15 -0500

    added completed list of commands to the script file except the extra credit

--- SystemInfo.sh
+++ SystemInfo.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 FILE="system_info_.txt"
 { 
-echo "Number of running processes excluding the threads: ";
+echo "Number of Running Processes Excluding The Threads: ";
 ps -A --no-headers | wc -l;
 echo -e;
-echo "Number of running processes including the threads: ";
+echo "Number of Running Processes Including The Threads: ";
 ps -AL --no-headers | wc -l;
 echo -e;
-echo "Linux kernel release: ";
+echo "Linux kernel Release: ";
 uname -r;
 echo -e;
+echo "Processor Type: ";
+lscpu | grep "Model name" | awk '{ $1=$2="";$0=$0;} NF=NF'
+echo -e;
+echo "Processor Description: ";
+lscpu | grep -E '^Arch|^Thread|^Core|^Socket|^CPU|^L'
+echo -e;
 echo "Available Disk Space: ";
-df -H | grep -E '^/dev' | awk '{ print $4 " " $1 }';
+df -H | grep -E '^/dev' | awk '{ print $4 " " $1}';
+echo -e;
+echo "Used Disk Space In Percentage: ";
+df -H | grep -E '^/dev' | awk '{ print $5 " " $1 }'
+echo -e;
+echo "Mac Address For The System: ";
+ip link | awk '/ether/ {print $2}'
 } > $FILE
